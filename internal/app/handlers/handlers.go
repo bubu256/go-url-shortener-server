ffdf908a609b9ffd736d958bb247b77eaa8ca799
rettopekim <bulostm@gmail.com>
2023-01-04 21:10:19 +0300

    added handler for /api/shorten

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -15,11 +16,21 @@ type Handlers struct {
 	service *shortener.Shortener
 }
 
+// структура для принятия данных в запросе из json
+type InputData struct {
+	URL string `json:"url"`
+}
+
+type OutputData struct {
+	Result string `json:"result"`
+}
+
 func New(service *shortener.Shortener) *Handlers {
 	NewHandlers := Handlers{}
 	NewHandlers.service = service
 	router := chi.NewRouter()
 	router.Post("/", NewHandlers.HandlerURLtoShort)
+	router.Post("/api/shorten", NewHandlers.HandlerApiShorten)
 	router.Get("/{ShortKey}", NewHandlers.HandlerShortToURL)
 	NewHandlers.Router = router
 	return &NewHandlers
@@ -58,4 +69,43 @@ func (h Handlers) HandlerShortToURL(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 	}
+}
+
+func (h *Handlers) HandlerApiShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Не удалось прочитать тело POST запроса.", http.StatusInternalServerError)
+		return
+	}
+	inputData := InputData{}
+	err = json.Unmarshal(body, &inputData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	shortKey, err := h.service.CreateShortURL(inputData.URL)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	shortURL := url.URL{
+		Scheme: "http",
+		Host:   r.Host,
+		Path:   shortKey,
+	}
+	outputData := OutputData{Result: shortURL.String()}
+	result, err := json.Marshal(outputData)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(result)
 }
--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -16,23 +16,27 @@ import (
 )
 
 func TestRouting(t *testing.T) {
+	// инициализация хранилища, сервиса и сервера
 	longURL := "https://translate.google.ru/?hl=ru&tab=wT&sl=ru&tl=en&text=%D0%A2%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5&op=translate"
 	initMap := map[string]string{"-testKey": longURL}
 	cfg := config.New()
 	cfg.DB.InitialData = initMap
-
 	dataStorage := storage.NewMapDBMutex(cfg.DB)
 	service := shortener.New(dataStorage)
 	handler := New(service)
 	srv := httptest.NewServer(handler.Router)
-	// клиент не следует по перенаправлениям
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
+			return http.ErrUseLastResponse // клиент не следует по перенаправлениям
 		},
 	}
-
 	defer srv.Close()
+
+	shortKeySeed42 := "/q0V3bS"
+	// jsonInput, err := json.Marshal(InputData{longURL})
+	// require.NoError(t, err)
+	// jsonOut, err := json.Marshal(OutputData{srv.URL + shortKeySeed42})
+	// require.NoError(t, err)
 
 	type want struct {
 		body       string
@@ -63,9 +67,20 @@ func TestRouting(t *testing.T) {
 		{
 			name: "create short link 201",
 			req:  req{method: "POST", url: "/", body: longURL},
-			// проверка body возможна токи при фиксации rand.seed в тесте
-			want: want{statusCode: http.StatusCreated, body: srv.URL + "/q0V3bS"},
+			// проверка body возможна только при фиксации rand.seed в тесте
+			want: want{statusCode: http.StatusCreated, body: srv.URL + shortKeySeed42},
 		},
+		// {
+		// 	name: "api/shorten json create link 201",
+		// 	req: req{
+		// 		method: "POST",
+		// 		url:    "/api/shorten",
+		// 		body:   string(jsonInput),
+
+		// 	},
+		// 	// проверка body возможна только при фиксации rand.seed в тесте
+		// 	want: want{statusCode: http.StatusCreated, body: string(jsonOut)},
+		// },
 	}
 
 	for _, d := range tt {
@@ -87,5 +102,49 @@ func TestRouting(t *testing.T) {
 			assert.Equal(t, d.want.body, string(body))
 		})
 	}
+}
 
+func TestHandlers_HandlerApiShorten(t *testing.T) {
+	cfg := config.New()
+	dataStorage := storage.NewMapDB(cfg.DB)
+	service := shortener.New(dataStorage)
+	handler := New(service)
+
+	type want struct {
+		body        string
+		statusCode  int
+		contentType string
+	}
+	type req struct {
+		body        string
+		contentType string
+	}
+	tests := []struct {
+		name string
+		req  req
+		want want
+	}{
+		{
+			name: "api json created link",
+			req:  req{contentType: "application/json", body: `{"url":"https://translate.google.ru/?hl=ru&tab=wT&sl=ru&tl=en&text=%D1%82%D0%B5%D1%81%D1%82%20%20%20&op=translate"}`},
+			want: want{statusCode: http.StatusCreated, contentType: "application/json", body: `{"result":"http://example.com/q0V3bS"}`},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rand.Seed(42)
+			// h := Handlers{}
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("POST", "/api/shorten", bytes.NewBufferString(tt.req.body))
+			r.Header.Set("Content-Type", "application/json")
+			// h := &Handlers{}
+			handler.HandlerApiShorten(w, r)
+			result := w.Result()
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+			body, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, tt.want.body, string(body))
+		})
+	}
 }
