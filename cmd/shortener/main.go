5b51e7d5454156608168f2a73daae84d1b18d579
rettopekim <bulostm@gmail.com>
2023-04-04 04:48:37 +0300

    added pprof to main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/bubu256/go-url-shortener-server/internal/app/handlers"
 	"github.com/bubu256/go-url-shortener-server/internal/app/shortener"
 	"github.com/bubu256/go-url-shortener-server/pkg/storage"
+
+	// "github.com/bubu256/go-url-shortener-server/profiles"
+	_ "net/http/pprof"
 )
 
 func main() {
@@ -17,6 +20,12 @@ func main() {
 	dataStorage := storage.New(cfg.DB, nil)
 	service := shortener.New(dataStorage, cfg.Service)
 	handler := handlers.New(service, cfg.Server)
+	// как лучше: так?
+	// handler.Router.Mount("/debug", profiles.Profiler())
+	// или так
+	go func() {
+		http.ListenAndServe(":6060", nil)
+	}()
 	log.Println("Сервер:", cfg.Server.ServerAddress)
 	log.Fatal(http.ListenAndServe(cfg.Server.ServerAddress, handler.Router))
 }
